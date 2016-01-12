f735e537e622e0223e2bf35a3d2882d07222f11f
jokemaan <nnhuo@qq.com>
2016-01-12 17:16:31 +0800

    add configurations

---
+++ www/config_default.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+__author__='Nina'
+
+'''
+Default configuarion
+'''
+
+configs={
+    'db': {
+        'host': '127.0.0.1',
+        'port': 3306,
+        'user': 'kok',
+        'password': 'kok',
+        'database': 'awesome'
+    },
+    'session': {
+        'secret': 'AwEsOmE'
+    }
+}
